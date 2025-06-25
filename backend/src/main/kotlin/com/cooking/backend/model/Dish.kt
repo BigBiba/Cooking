cd91e52bf07b6648009ba0244be120ed12ddff5b
danilgordienko <danilgordienko212@gmail.com>
2025-06-25 17:42:24 +0300

    поправил создание и отображение рецептов
    теперь работает :)

--- backend/src/main/kotlin/com/cooking/backend/model/Dish.kt
+++ backend/src/main/kotlin/com/cooking/backend/model/Dish.kt
@@ -7,26 +7,26 @@ import java.time.LocalDateTime
 
 @Entity
 @Table(name = "dishes")
-data class Dish(
+data class Dish @JvmOverloads constructor(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Int = 0,
 
-    val title: String,
-    val description: String,
+    val title: String = "",
+    val description: String = "",
 
-    val ingredients: String,
+    val ingredients: String = "",
 
-    val recipe: String,
-    val category: String,
+    val recipe: String = "",
+    val category: String = "",
 
     val updated_at: LocalDateTime = LocalDateTime.now(),
     val created_at: LocalDateTime = LocalDateTime.now(),
 
     @ManyToOne
     @JoinColumn(name = "creatorId", referencedColumnName = "id")
-    val creator: User,
+    val creator: User = User(),
 
-    val photoUrl: String,
+    val photoUrl: String = "",
 
     @OneToMany(mappedBy = "dish", cascade = [CascadeType.ALL])
     val favorites: List<Favorite> = listOf()
